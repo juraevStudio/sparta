cad7d70f9122914f1352b4d121089b8a0a1db521
Otabek <blogitcodex@gmail.com>
2025-06-01 23:59:04 +0500

    finish page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// // Lenis
+const lenis = new Lenis({
+    wrapper: document.querySelector('.scroll-container'),
+    lerp: 0.1,
+    duration: 2.2,
+    smoothTouch: true
+});
+lenis.on('scroll', e => {
+    console.log(e);
+});
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+const animatedElements = document.querySelectorAll('.animate-on-scroll');
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            observer.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove('is-visible');
+        }
+    });
+});
+animatedElements.forEach(el => observer.observe(el));
+
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > 10) {
+        $('.header').addClass("fixed")
+    } else {
+        $('.header').removeClass("fixed")
+    }
+});
+
 $(".header__burgir").click(function() {
     
     if($(".header__burgir").hasClass("active")) {
